261df4e527f2bcfeb608949ca67a3d1535b40448
logic-fabric <lowic@logic-fabric.fr>
2023-07-06 07:42:48 +0200

    feature: compute and display tip & total per person

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const billAmountInput = document.getElementById("bill-amount");
+
 const nbOfPeopleInput = document.getElementById("nb-of-people");
 const nbOfPeopleError = document.getElementById("nb-of-people-error");
 const nbOfPeopleContainer = document.getElementById("nb-of-people-container");
+
+const tipAmountPerPersonContainer = document.getElementById(
+  "tip-amount-per-person"
+);
+const totalPerPersonContainer = document.getElementById("total-per-person");
+
+let tipPercentage = 0; // as a number between 0 and 1
 
 billAmountInput.addEventListener("change", checkNbOfPeople);
 nbOfPeopleInput.addEventListener("change", checkNbOfPeople);
@@ -14,6 +22,14 @@ function checkNbOfPeople() {
     if (isValidNbOfPeople(nbOfPeople)) {
       nbOfPeopleContainer.classList.remove("card__input-field--error");
       nbOfPeopleError.classList.remove("show-error");
+
+      const tipAmountPerPerson = getTipAmountPerPerson();
+      const totalPerPerson = getTotalPerPerson();
+
+      tipAmountPerPersonContainer.textContent = `${formatPrice(
+        tipAmountPerPerson
+      )}`;
+      totalPerPersonContainer.textContent = `${formatPrice(totalPerPerson)}`;
     } else {
       nbOfPeopleContainer.classList.add("card__input-field--error");
       nbOfPeopleError.classList.add("show-error");
@@ -35,4 +51,25 @@ function isValidAmount(amount) {
 
 function isValidNbOfPeople(nbOfPeople) {
   return !isNaN(nbOfPeople) && nbOfPeople > 0;
+}
+
+function getTipAmountPerPerson() {
+  const billAmount = getBillAmount();
+  const nbOfPeople = getNbOfPeople();
+
+  return (billAmount * tipPercentage) / nbOfPeople;
+}
+
+function getTotalPerPerson() {
+  const billAmount = getBillAmount();
+  const nbOfPeople = getNbOfPeople();
+
+  return billAmount / nbOfPeople;
+}
+
+function formatPrice(price) {
+  const strPrice = price.toFixed(2).toString();
+  const [integerPart, decimalPart] = strPrice.split(".");
+
+  return `${integerPart}.${decimalPart}`;
 }
